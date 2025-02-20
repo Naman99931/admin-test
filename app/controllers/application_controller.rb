7fe29bfcfbc12ba4dca81d513c7227b4163958ff
Naman99931 <naman99931574@gmail.com>
2025-02-20 11:46:00 +0530

    seted up devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_if_signed_in
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
+  end
+
+  def redirect_if_signed_in
+    if user_signed_in? && request.path == root_path
+      # redirect_to posts_path
+    end
+  end
 end
